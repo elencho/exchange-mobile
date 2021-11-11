358a959df89ecc3116816c74c7a7be7729c77c1b
Vakhtang Elisabedashvili <velisa@cryptx.com>
2021-11-11 12:46:07 +0400

    started using custom fonts

--- App.js
+++ App.js
@@ -5,6 +5,7 @@ import { applyMiddleware, createStore } from 'redux';
 import { Provider } from 'react-redux';
 import createSagaMiddleware from 'redux-saga';
 import { composeWithDevTools } from 'redux-devtools-extension';
+import { useFonts } from 'expo-font';
 
 import { reducer } from './src/redux/rootReducer';
 import mySaga from './src/redux/sagas';
@@ -19,12 +20,14 @@ const store = createStore(
 sagaMiddleware.run(mySaga);
 
 export default function App() {
-  const statusBarCond = () => {
-    if (Platform.OS === 'android') {
-      return <StatusBar style="light" />;
-    }
-    return <SafeAreaView style={styles.statusBar} />;
-  };
+  const [loaded] = useFonts({
+    Ubuntu_Regular: require('./src/assets/fonts/Ubuntu_Regular.ttf'),
+    Ubuntu_Medium: require('./src/assets/fonts/Ubuntu_Medium.ttf'),
+  });
+
+  if (!loaded) {
+    return null;
+  }
 
   return (
     <Provider store={store}>
