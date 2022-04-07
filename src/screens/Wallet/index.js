bdf193c0d91382a92c2a0a492a011b67d25b41cf
Vakhtang Elisabedashvili <velisa@cryptx.com>
2022-04-07 16:48:06 +0400

    bug fix currency cleanup

--- src/screens/Wallet/index.js
+++ src/screens/Wallet/index.js
@@ -1,13 +1,24 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { StyleSheet, View } from 'react-native';
+import { useDispatch } from 'react-redux';
+
 import Background from '../../components/Background';
 import TopRow from '../../components/TransactionHistory/TopRow';
 import Headline from '../../components/TransactionHistory/Headline';
 import CurrencySwitch from '../../components/Wallet/CurrencySwitch';
 import TotalBalance from '../../components/Wallet/TotalBalance';
 import BalancesList from '../../components/Wallet/BalancesList';
+import { chooseCurrency, setAbbr } from '../../redux/transactions/actions';
 
 export default function Wallet() {
+  const dispatch = useDispatch();
+  useEffect(() => {
+    return () => {
+      dispatch(chooseCurrency('Show All Currency'));
+      dispatch(setAbbr(null));
+    };
+  }, []);
+
   return (
     <Background>
       <TopRow />
