e07c7d8df77d000dc05948b6ecfeadf8a2ca76d8
Davit Kvartskhava <dkvarts@cryptx.com>
2022-12-26 12:16:36 +0400

    Another minor change in appcenter post clone script

--- appcenter-post-clone.sh
+++ appcenter-post-clone.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
+cd ios
+echo $(pwd)
 
 echo "Uninstalling all CocoaPods versions"
 sudo gem uninstall cocoapods --all --executables
 
-cd ios
-echo $(pwd)
 COCOAPODS_VER=`sed -n -e 's/^COCOAPODS: \([0-9.]*\)/\1/p' Podfile.lock`
 
 echo "Installing CocoaPods version $COCOAPODS_VER"
