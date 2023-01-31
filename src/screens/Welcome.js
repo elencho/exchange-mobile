6bef48ebcb1b0f66ce094a5e78758f9f0853f19b
Elene Botchoradze <ebotch@cryptx.com>
2023-01-31 18:36:52 +0400

    push to 98 version

--- src/screens/Welcome.js
+++ src/screens/Welcome.js
@@ -81,7 +81,7 @@ export default function Welcome({ navigation }) {
           <>
             <Logo style={styles.logo} />
             <AppText header style={styles.primary}>
-              Welcome to Cryptal 97
+              Welcome to Cryptal 98
             </AppText>
 
             {/* <AppText style={styles.secondary}>{auth}</AppText> */}
