dba2df93b7761da817694d55bba5ab311ce274a1
Zuka <zuja@Zukas-MacBook-Pro.local>
2023-05-04 16:33:34 +0400

    [CAP-773]: Fix instant trade transactions amount style

--- src/components/InstantTrade/Trade.js
+++ src/components/InstantTrade/Trade.js
@@ -57,11 +57,12 @@ export default function Trade({ trade }) {
         <AppText style={[styles.primary, { marginRight: 10 }]} medium body>
           {baseCurrency} - {quoteCurrency}
         </AppText>
-        <View style={{ alignItems: 'flex-end' }}>
+        <View style={styles.amount}>
           <AppText style={styles.secondary} subtext>
             {cumulativeCost} {quoteCurrency}
           </AppText>
           <AppText subtext style={styles.primary}>
+            {' / '}
             {size} {baseCurrency}
           </AppText>
         </View>
@@ -122,5 +123,8 @@ const styles = StyleSheet.create({
   status: {
     flexDirection: 'row',
     alignItems: 'center',
+  },
+  amount: {
+    flexDirection: 'row',
   },
 });
