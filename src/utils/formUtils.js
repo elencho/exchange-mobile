b8e3b1f0714842c14c5fbde05d909633c4026a22
Zuka <zuja@Zukas-MacBook-Pro.local>
2023-05-17 12:43:05 +0400

    Fix amount input validations

--- src/utils/formUtils.js
+++ src/utils/formUtils.js
@@ -13,3 +13,39 @@ export const validateScale = (input, scale) => {
   }
   return false;
 };
+
+export const handleAmountInput = (
+  text,
+  regex,
+  scale,
+  setMaxLength,
+  setAmount
+) => {
+  const getMaxLength = (replacedAmount) => {
+    const factoredDigit = Math.trunc(replacedAmount);
+    const factoredDigitLength = parseFloat(factoredDigit.toString().length);
+    setMaxLength(factoredDigitLength + parseFloat(scale) + 1);
+  };
+
+  const replacedAmount = text?.trim().replace(',', '.');
+  if (!regex.test(replacedAmount) && replacedAmount) {
+    return;
+  }
+
+  if (!validateScale(replacedAmount, scale)) {
+    return;
+  }
+
+  const startsWithZeroNoDecimal =
+    replacedAmount[1] && replacedAmount[0] === '0' && replacedAmount[1] !== '.';
+  const parts = replacedAmount.split('.');
+  if (parts.length === 2) {
+    getMaxLength(replacedAmount);
+    setAmount(replacedAmount ? parts[0].substr(0, 14) + '.' + parts[1] : 0);
+  } else if (startsWithZeroNoDecimal) {
+    setMaxLength(2);
+  } else {
+    setMaxLength(14);
+    setAmount(replacedAmount ? parts[0].substr(0, 13) : 0);
+  }
+};
