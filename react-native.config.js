06a2fadcd2fe011efbb8168053ddd84a31bf3652
Zuka <zuja@Zukas-MacBook-Pro.local>
2023-05-29 13:15:33 +0400

    Fix: removed package

--- react-native.config.js
+++ react-native.config.js
@@ -2,6 +2,6 @@ module.exports = {
   dependencies: {
     ...(process.env.NO_FLIPPER
       ? { 'react-native-flipper': { platforms: { ios: null } } }
-      : {}),
+      : { 'react-native-flipper': { platforms: { ios: null } } }),
   },
 };
