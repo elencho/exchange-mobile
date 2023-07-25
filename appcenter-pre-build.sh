61fc5cef3e1d77ab9fae7d4a05d043b41f25bb96
elene_botchoradze <ebotch@cryptx.com>
2023-07-25 12:57:18 +0400

    fix crash

--- appcenter-pre-build.sh
+++ appcenter-pre-build.sh
@@ -4,33 +4,3 @@
 
 cd $APPCENTER_SOURCE_DIRECTORY && cd ios && rm -rf Pods && pod cache clean --all && pod update && cd ..
 
-#!/usr/bin/env bash
-
-# Function to copy the appropriate Firebase configuration files
-copy_firebase_config() {
-    if [ "$1" == "master" ]; then
-
-        cp $APPCENTER_SOURCE_DIRECTORY/ios/GoogleService-info-prod.plist GoogleService-info.plist
-        cp $APPCENTER_SOURCE_DIRECTORY/android/google-service-prod.json google-service.json
-    else
-   
-        cp $APPCENTER_SOURCE_DIRECTORY/ios/GoogleService-info-dev.plist GoogleService-info.plist
-        cp $APPCENTER_SOURCE_DIRECTORY/android/google-service-dev.json google-service.json
-    fi
-}
-
-# Get the current branch name from the environment variable APPCENTER_BRANCH
-branch="$APPCENTER_BRANCH"
-
-# Check if the branch name is empty (in case APPCENTER_BRANCH is not set)
-if [ -z "$branch" ]; then
-    echo "APPCENTER_BRANCH is not set. Exiting."
-    exit 1
-fi
-
-# Call the function to copy the appropriate Firebase configuration files
-copy_firebase_config "$branch"
-
-# Display a message indicating which .env and Firebase configuration files are used
-
-printf "\nFirebase configuration files used: GoogleService-info.plist and google-service.json\n"
