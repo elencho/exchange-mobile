e67251c1b9779798dc45d4eec6c88d79df99f75a
Zuka <zuja@Zukas-MacBook-Pro.local>
2023-07-31 16:06:29 +0200

    [CAP-976]: Refactored consoles

--- src/utils/fetchTrades.js
+++ src/utils/fetchTrades.js
@@ -10,23 +10,8 @@ import {
 } from '../constants/api';
 
 export const fetchTrades = async (params) => {
-  console.log(
-    'params',
-    params,
-    encodeURIComponent(new URLSearchParams(params))
-  );
-
-  try {
-    const data = await axios.get(TRADES_URL_PAGED, {
-      params,
-    });
-    console.log(data);
-
-    return data;
-  } catch (err) {
-    console.error(err);
-    return {};
-  }
+  const data = await axios.get(TRADES_URL_PAGED, { params });
+  return data?.data;
 };
 
 export const fetchOffers = async () => {
