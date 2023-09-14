361f5edb00174e97cfb58ad3bad0e165dbcc8f8f
elene_botchoradze <ebotch@cryptx.com>
2023-09-14 16:58:28 +0400

    [CAP-1102]: margin and paddings fix

--- src/components/UserProfile/PasswordModal.js
+++ src/components/UserProfile/PasswordModal.js
@@ -172,7 +172,7 @@ export default function PasswordModal() {
           />
 
           <AppInput
-            style={[styles.inputContainer, { marginTop: 28 }]}
+            style={[styles.inputContainer, { marginTop: 18 }]}
             label="Current Password"
             autoCapitalize={'none'}
             secureTextEntry={secure}
@@ -242,7 +242,7 @@ const styles = StyleSheet.create({
     marginRight: 15,
   },
   error: {
-    marginBottom: 15,
+    marginTop: 10,
   },
   inputContainer: {
     marginBottom: 20,
