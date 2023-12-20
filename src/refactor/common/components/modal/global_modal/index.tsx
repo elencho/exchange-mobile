6409c42dcdc8b73efcf2ef1dae383e3f2931dc3b
elene_botchoradze <ebotch@cryptx.com>
2023-12-21 01:30:56 +0400

    [CAP-1022]: modal design

--- src/refactor/common/components/modal/global_modal/index.tsx
+++ src/refactor/common/components/modal/global_modal/index.tsx
@@ -8,13 +8,18 @@ import React, {
 } from 'react'
 import {
 	View,
-	TouchableOpacity,
 	Text,
 	StyleSheet,
 	ImageBackground,
+	Linking,
+	Dimensions,
+	ScrollView,
 } from 'react-native'
 import Modal from 'react-native-modal'
 import CloseIcon from '@components/close-button'
+import { Theme, useTheme } from '@theme/index'
+
+import { AppButton } from '@components/button'
 
 type ModalContextType = {
 	showModal: (content: ContentType) => void
@@ -33,7 +38,17 @@ const ModalContext = createContext<ModalContextType | undefined>(undefined)
 
 export const ModalProvider = ({ children }: { children: ReactNode }) => {
 	const [isModalVisible, setModalVisible] = useState(false)
-	const [modalContent, setModalContent] = useState<ContentType | null>(null)
+	const { styles } = useTheme(_styles)
+	// TODO: remove, left for testing purpose
+	const [modalContent, setModalContent] = useState<ContentType>({
+		title: 'Title',
+		redirectUrl: 'google.com',
+		callToAction: 'buy it now',
+		description:
+			'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt uti labore et dolore magna aliquyam erat, sed diamsd voluptua. At vero eos et accusam et justo duo asor dolores et ea rebum. Stet clita kasd gubergren, no Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquy. Find out more',
+		banner:
+			'https://s.yimg.com/ny/api/res/1.2/8y6wUwbKd.9Wbx_MS7t.Vw--/YXBwaWQ9aGlnaGxhbmRlcjt3PTY0MDtoPTUwNQ--/https://media.zenfs.com/en-US/homerun/fx_empire_176/716acf40f2f984c032e885a3b3a0cf62',
+	})
 
 	const showModal = (content: ContentType) => {
 		setModalContent(content)
@@ -45,35 +60,74 @@ export const ModalProvider = ({ children }: { children: ReactNode }) => {
 		setModalVisible(false)
 	}
 
-	const Background: FC<PropsWithChildren> = ({ children }) => {
-		return !modalContent?.banner ? (
-			<ImageBackground
-				// imageStyle={imageStyle}
-				style={styles.imageStyle}
-				source={{
-					uri: 'https://s.yimg.com/ny/api/res/1.2/8y6wUwbKd.9Wbx_MS7t.Vw--/YXBwaWQ9aGlnaGxhbmRlcjt3PTY0MDtoPTUwNQ--/https://media.zenfs.com/en-US/homerun/fx_empire_176/716acf40f2f984c032e885a3b3a0cf62',
-				}}
-				children={children}
-			/>
+	const BackgroundWrapper: FC<PropsWithChildren> = ({ children }) => {
+		return modalContent?.banner ? (
+			<View style={styles.imageBckWrapper}>
+				<ImageBackground
+					imageStyle={styles.imageFillStyle}
+					resizeMode="cover"
+					style={styles.imageStyle}
+					source={{
+						uri: modalContent?.banner,
+					}}>
+					<CloseIcon
+						style={{ marginTop: 60, marginRight: 30 }}
+						onPress={hideModal}
+					/>
+				</ImageBackground>
+
+				{children}
+			</View>
 		) : (
-			<Background children={children} />
+			<View style={styles.imageBckWrapper}>
+				<CloseIcon
+					style={{ marginTop: 60, marginRight: 30 }}
+					onPress={hideModal}
+				/>
+				{children}
+			</View>
 		)
+	}
+
+	const onPress = () => {
+		Linking.openURL(modalContent?.redirectUrl)
 	}
 
 	return (
 		<ModalContext.Provider value={{ showModal, hideModal }}>
 			{children}
 			<Modal
+				propagateSwipe={true}
 				useNativeDriver
 				isVisible={true}
 				onDismiss={hideModal}
 				coverScreen
 				animationOutTiming={500}
 				backdropTransitionInTiming={300}
-				style={{ margin: 0, padding: 0 }}>
-				<Background>
-					<CloseIcon onPress={hideModal} />
-				</Background>
+				style={{ margin: 0, justifyContent: 'flex-end' }}>
+				<View style={styles.scrollWrapper}>
+					<ScrollView bounces={false} showsVerticalScrollIndicator={false}>
+						<BackgroundWrapper>
+							<View
+								style={
+									modalContent?.banner
+										? styles.contentWrapper
+										: styles.contentWrapperWithoutBanner
+								}>
+								<Text style={styles.title}>{modalContent.title}</Text>
+								<Text style={styles.descr}>{modalContent.description}</Text>
+							</View>
+							{modalContent?.callToAction && modalContent?.redirectUrl && (
+								<AppButton
+									variant="primary"
+									text={modalContent.callToAction}
+									onPress={onPress}
+									style={styles.button}
+								/>
+							)}
+						</BackgroundWrapper>
+					</ScrollView>
+				</View>
 			</Modal>
 		</ModalContext.Provider>
 	)
@@ -87,18 +141,65 @@ export const useModal = (): ModalContextType => {
 	return context
 }
 
-const styles = StyleSheet.create({
-	modalContainer: {
-		flex: 1,
-		justifyContent: 'center',
-		alignItems: 'center',
-	},
-	modalContent: {
-		backgroundColor: 'white',
-		padding: 20,
-		borderRadius: 10,
-		elevation: 5,
-	},
-	imageStyle: { height: '50%', width: '100%', backgroundColor: 'red' },
-	container: {},
-})
+const WINDOW_HEIGHT = Dimensions.get('window').height
+
+const _styles = (theme: Theme) =>
+	StyleSheet.create({
+		modalContainer: {
+			flex: 1,
+			justifyContent: 'center',
+			alignItems: 'center',
+		},
+		modalContent: {
+			backgroundColor: 'white',
+			padding: 20,
+			borderRadius: 10,
+			elevation: 5,
+		},
+		imageStyle: {
+			height: WINDOW_HEIGHT / 2,
+			width: '100%',
+		},
+		container: {},
+		imageFillStyle: {
+			height: '100%',
+		},
+		imageBckWrapper: {
+			backgroundColor: theme.color.backgroundPrimary,
+		},
+		contentWrapper: {
+			marginTop: 24,
+			marginHorizontal: 20,
+			paddingBottom: 154,
+			minHeight: WINDOW_HEIGHT / 2,
+		},
+		contentWrapperWithoutBanner: {
+			marginTop: 24,
+			marginHorizontal: 20,
+			minHeight: WINDOW_HEIGHT - 154,
+		},
+		title: {
+			fontSize: 24,
+			lineHeight: 28,
+			fontFamily: theme.font.medium,
+			color: '#FFFFFF',
+		},
+		descr: {
+			fontSize: 14,
+			lineHeight: 18,
+			marginTop: 16,
+			fontFamily: theme.font.medium,
+			color: '#ccd9dd',
+		},
+		scrollWrapper: {
+			backgroundColor: theme.color.backgroundPrimary,
+			flex: 1,
+		},
+		button: {
+			position: 'absolute',
+			bottom: 44,
+			width: '90%',
+
+			alignSelf: 'center',
+		},
+	})
