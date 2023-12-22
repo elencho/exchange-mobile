84fce4a954ac3b84a7032a95adbb95d445b71aa7
Zuka <zjakel@cryptx.com>
2023-12-22 19:30:59 +0400

    [CAP-1479]: Add animation for wallet switcher

--- src/components/Wallet/WalletSwitcher.js
+++ src/components/Wallet/WalletSwitcher.js
@@ -1,10 +1,19 @@
-import React, { useEffect, useState } from 'react'
-import { Pressable, StyleSheet, View } from 'react-native'
+import React, { useEffect, useRef, useState } from 'react'
+import {
+	Dimensions,
+	FlatList,
+	Pressable,
+	ScrollView,
+	StyleSheet,
+	View,
+} from 'react-native'
 import { useDispatch, useSelector } from 'react-redux'
 import colors from '../../constants/colors'
 import { setCard } from '../../redux/trade/actions'
 import { setNetwork, setWalletTab } from '../../redux/wallet/actions'
 import AppText from '../AppText'
+
+const BUTTON_WIDTH = 120
 
 export default function WalletSwitcher() {
 	const dispatch = useDispatch()
@@ -15,13 +24,11 @@ export default function WalletSwitcher() {
 	} = state
 
 	const cur = currentBalanceObj
-	const array = ['Deposit', 'Withdrawal']
+	const array = ['Deposit', 'Withdrawal', 'Whitelist']
 	const [switchers, setSwitchers] = useState(array)
 
 	useEffect(() => {
-		if (cur.type === 'CRYPTO') {
-			setSwitchers([...array, 'Whitelist'])
-		} else if (
+		if (
 			cur.type === 'FIAT' &&
 			(cur.depositMethods.ECOMMERCE || cur.withdrawalMethods.ECOMMERCE)
 		) {
@@ -48,6 +55,11 @@ export default function WalletSwitcher() {
 		}
 	}
 
+	const scrollViewRef = useRef()
+	useEffect(() => {
+		scrollViewRef.current.scrollTo({ x: 0, y: 0 })
+	}, [currentBalanceObj])
+
 	const buttonStyle = (f) => {
 		return {
 			backgroundColor:
@@ -65,17 +77,32 @@ export default function WalletSwitcher() {
 	}
 
 	return (
-		<View style={styles.row}>
-			{switchers.map((f, i) => (
-				<Pressable
-					key={f}
-					style={[styles.button, buttonStyle(f)]}
-					onPress={() => handleWalletTab(f)}>
-					<AppText body style={textStyle(f)}>
-						{f}
-					</AppText>
-				</Pressable>
-			))}
+		<View>
+			<ScrollView
+				ref={scrollViewRef}
+				style={styles.row}
+				contentContainerStyle={{
+					width:
+						switchers.length > 3
+							? BUTTON_WIDTH * switchers.length + 20
+							: BUTTON_WIDTH * switchers.length,
+					paddingRight: 5,
+				}}
+				horizontal
+				scrollEnabled={switchers.length > 3}
+				bounces={true}
+				showsHorizontalScrollIndicator={false}>
+				{switchers.map((f, i) => (
+					<Pressable
+						key={f}
+						style={[styles.button, buttonStyle(f)]}
+						onPress={() => handleWalletTab(f)}>
+						<AppText body style={textStyle(f)}>
+							{f}
+						</AppText>
+					</Pressable>
+				))}
+			</ScrollView>
 		</View>
 	)
 }
@@ -86,10 +113,10 @@ const styles = StyleSheet.create({
 		borderRadius: 40,
 		alignItems: 'center',
 		justifyContent: 'center',
+		marginRight: 6,
+		maxWidth: BUTTON_WIDTH,
 	},
 	row: {
-		flexDirection: 'row',
-		justifyContent: 'space-between',
 		marginTop: 22,
 		marginBottom: 28,
 	},
