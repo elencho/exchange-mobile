e8c5d5d34c103fe814c41941d90e62b75a763976
elene_botchoradze <ebotch@cryptx.com>
2023-12-26 17:20:25 +0400

    [CAP-1484]:send fcm token to back and delete

--- src/screens/useNotificationPermissions.js
+++ src/screens/useNotificationPermissions.js
@@ -3,27 +3,48 @@ import React, { useEffect } from 'react'
 import { PermissionsAndroid } from 'react-native'
 import { IS_ANDROID, IS_IOS } from '../constants/system'
 import { notificationSubscribe } from '@app/refactor/redux/profile/profileApi'
+import notifee, { AuthorizationStatus } from '@notifee/react-native'
 
-const useNotificationPermissions = () => {
+const useNotificationPermissions = async () => {
+	const settings = await notifee.getNotificationSettings()
+
 	const requestUserPermissionIOS = async () =>
-		await messaging().requestPermission()
+		await messaging()
+			.requestPermission()
+			.then((value) => {
+				if (value === AuthorizationStatus.AUTHORIZED) {
+					checkFcm()
+				}
+			})
+
+	const checkPermission = () => {
+		if (settings.authorizationStatus == AuthorizationStatus.DENIED) {
+			if (IS_ANDROID) {
+				requestPermissionsAndroid()
+			} else {
+				requestUserPermissionIOS()
+			}
+		}
+	}
 
 	const requestPermissionsAndroid = () =>
 		PermissionsAndroid.request(
 			PermissionsAndroid.PERMISSIONS.POST_NOTIFICATIONS
-		)
+		).then((value) => {
+			if (value === 'granted') {
+				checkFcm()
+			}
+		})
 
 	const checkFcm = async () => {
 		const token = await messaging().getToken()
 		if (token) {
 			notificationSubscribe(token)
-		} else {
-			IS_ANDROID ? requestPermissionsAndroid() : requestUserPermissionIOS()
 		}
 	}
 
 	useEffect(() => {
-		checkFcm()
+		checkPermission()
 	}, [])
 
 	return {}
