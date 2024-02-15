7c23a4403ddb475f99f0f2761665753efab2a570
elene_botchoradze <ebotch@cryptx.com>
2024-02-15 16:39:03 +0400

    [CAP-1647]: phone number fix

--- src/refactor/screens/profile/user-profile/components/modals/user-profile/phone-number-modal/use-phone-number-modal.tsx
+++ src/refactor/screens/profile/user-profile/components/modals/user-profile/phone-number-modal/use-phone-number-modal.tsx
@@ -132,10 +132,11 @@ export const usePhoneNumberModal = ({
 
 	const handleSave = () => {
 		setGeneralErrorData(null)
-		
+		setError({
+			phoneNumber: false,
+			verificationCode: false,
+		})
 		if (
-			error.phoneNumber ||
-			(error.verificationCode && otpType === OTPTypes.SMS) ||
 			!chosenCountry?.phoneCode ||
 			(!(verificationCode?.trim()?.length > 0) && otpType === OTPTypes.SMS) ||
 			!(phoneNumber?.trim()?.length > 0)
