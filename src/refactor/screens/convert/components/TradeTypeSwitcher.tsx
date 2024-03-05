8eaba2ddc7fc8b902a4955bacca109a32ed3a3ca
LevanGelashvili <lgelas@cryptx.com>
2024-03-05 16:53:19 +0400

    [CAP-1498] BuySell Animation

--- src/refactor/screens/convert/components/TradeTypeSwitcher.tsx
+++ src/refactor/screens/convert/components/TradeTypeSwitcher.tsx
@@ -1,8 +1,14 @@
 import { convertColors } from '@app/refactor/screens/convert/util'
 import AppText from '@components/text'
 import { Theme, useTheme } from '@theme/index'
-import React from 'react'
+import React, { useEffect, useRef } from 'react'
 import { Pressable, StyleSheet, View } from 'react-native'
+import Animated, {
+	Easing,
+	useAnimatedStyle,
+	useSharedValue,
+	withTiming,
+} from 'react-native-reanimated'
 
 type Props = {
 	selectedType: TradeType
@@ -13,43 +19,64 @@ const TradeTypeSwitcher = ({ selectedType, onTypeChanged }: Props) => {
 	const { styles, theme } = useTheme(_styles)
 	const tradesToDraw: TradeType[] = ['Buy', 'Sell']
 
-	const chooseBackgroundColor = (type: TradeType) => {
-		if (type === selectedType) {
-			return type === 'Buy' ? convertColors.buy : convertColors.sell
-		}
-		return theme.color.backgroundSecondary
-	}
+	const containerWidthRef = useRef(0)
+	const xPosition = useSharedValue(0)
 
-	const chooseTextColor = (type: TradeType) => {
-		return type === selectedType
-			? theme.color.textPrimary
-			: theme.color.textThird
-	}
+	useEffect(() => {
+		xPosition.value = selectedType === 'Buy' ? 0 : containerWidthRef.current / 2
+	}, [selectedType])
+
+	const animStyle = useAnimatedStyle(() => {
+		return {
+			transform: [
+				{
+					translateX: withTiming(xPosition.value, {
+						duration: 250,
+						easing: Easing.linear,
+					}),
+				},
+			],
+			position: 'absolute',
+			width: '50%',
+			height: '100%',
+			borderRadius: 40,
+			backgroundColor:
+				selectedType === 'Buy' ? convertColors.buy : convertColors.sell,
+		}
+	})
 
 	return (
-		<View style={styles.container}>
-			{tradesToDraw.map((type: TradeType) => (
-				<Pressable
-					key={type}
-					style={[
-						styles.button,
-						{ backgroundColor: chooseBackgroundColor(type) },
-					]}
-					onPress={() => {
-						type !== selectedType && onTypeChanged(type)
-					}}>
-					<AppText
-						style={[
-							styles.text,
-							{
-								color: chooseTextColor(type),
-							},
-						]}
-						variant="l">
-						{type}
-					</AppText>
-				</Pressable>
-			))}
+		<View
+			style={styles.container}
+			onLayout={(e) => {
+				containerWidthRef.current = e.nativeEvent.layout.width
+			}}>
+			<Animated.View style={animStyle}></Animated.View>
+			<View
+				style={{
+					height: '100%',
+					flexDirection: 'row',
+				}}>
+				{tradesToDraw.map((type: TradeType) => (
+					<Pressable
+						key={type}
+						style={styles.button}
+						onPress={() => {
+							type !== selectedType && onTypeChanged(type)
+						}}>
+						<AppText
+							style={{
+								color:
+									type === selectedType
+										? theme.color.textPrimary
+										: theme.color.textThird,
+							}}
+							variant="l">
+							{type}
+						</AppText>
+					</Pressable>
+				))}
+			</View>
 		</View>
 	)
 }
@@ -61,16 +88,11 @@ const _styles = (theme: Theme) =>
 			height: 40,
 			borderRadius: 40,
 			backgroundColor: theme.color.backgroundSecondary,
-			flexDirection: 'row',
 		},
 		button: {
 			justifyContent: 'center',
 			alignItems: 'center',
-			borderRadius: 40,
 			flex: 1,
-		},
-		text: {
-			color: theme.color.textPrimary,
 		},
 	})
 
